f7b9c00e2514efb0d29c30e00180931eaa08d85a
jD4rk <jdm0b1l3@gmail.com>
2016-04-22 11:22:47 +0200

    Implement Dependency Injection with Dagger2
    Previous implementation with Stetho of Network call debugging only on the "debug Version" is still present.
    
    NOTE:
    Possible duplicate code because to archive the above result I just crete different "Module" and "Component" Dagger for each BuildTypes
    
    **Let's try in a new branch to get it in a different way only with dagger **

--- app/src/release/java/it/jdark/android/retrofit/MyApplication.java
+++ app/src/release/java/it/jdark/android/retrofit/MyApplication.java
@@ -3,13 +3,29 @@ package it.jdark.android.retrofit;
 import android.app.Application;
 import android.util.Log;
 
+import it.jdark.android.retrofit.dipendentInjection.DaggerRetrofitComponent;
+import it.jdark.android.retrofit.dipendentInjection.RetrofitComponent;
+import it.jdark.android.retrofit.dipendentInjection.RetrofitModule;
+
 /**
  * Created by jDark on 12/04/16.
  */
 public class MyApplication extends Application {
+
+    private final String LOG = getClass().getSimpleName();
+    private final String URL = "http://api.openweathermap.org/data/2.5/";
+
+    RetrofitComponent component;
+
+
+    public RetrofitComponent getComponent() {
+        return component;
+    }
+
     @Override
     public void onCreate() {
+        Log.d(LOG, "onCreate");
         super.onCreate();
-        Log.d(MainActivity.LOG, "Application - onCreate");
+        component = DaggerRetrofitComponent.builder().retrofitModule(new RetrofitModule(URL)).build();
     }
 }
